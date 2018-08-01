7b60cc4211f53fa08e4f8d4a739ed594d07c92aa
Elad Nava <eladnava@gmail.com>
2018-08-01 11:27:40 +0800

    Reformat background.js code

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,20 +1,17 @@
 // Browser action clicked
-chrome.browserAction.onClicked.addListener(function(tab)
-{
+chrome.browserAction.onClicked.addListener(function (tab) {
     // Get enabled flag
-    chrome.storage.local.get('enabled', function(result)
-    {
-		// First time?
-		if ( result.enabled == undefined )
-		{
-			result.enabled = true;
-		}
-		
+    chrome.storage.local.get('enabled', function (result) {
+        // First time?
+        if (result.enabled == undefined) {
+            result.enabled = true;
+        }
+
         // Toggle enabled status
         var enabled = !result.enabled;
 
         // Save it using the Chrome extension storage API.
-        chrome.storage.local.set({'enabled': enabled});
+        chrome.storage.local.set({ 'enabled': enabled });
 
         // Refresh extension button
         refreshIcon(tab, enabled);
@@ -25,25 +22,21 @@ chrome.browserAction.onClicked.addListener(function(tab)
 });
 
 // Refresh the browser action icon based on enabled/disabled state
-function refreshIcon(tab, enabled)
-{
+function refreshIcon(tab, enabled) {
     // Get enabled flag
-    chrome.storage.local.get('enabled', function(result)
-    {
+    chrome.storage.local.get('enabled', function (result) {
         // Extension enabled?
-        if (result.enabled || result.enabled == undefined)
-        {
-            chrome.browserAction.setIcon({path: "assets/img/icon48.png", tabId: tab.id});
+        if (result.enabled || result.enabled == undefined) {
+            chrome.browserAction.setIcon({ path: "assets/img/icon48.png", tabId: tab.id });
         }
-        else
-        {
-            chrome.browserAction.setIcon({path: "assets/img/icon48_off.png", tabId: tab.id});
+        else {
+            // Extension disabled
+            chrome.browserAction.setIcon({ path: "assets/img/icon48_off.png", tabId: tab.id });
         }
     });
 }
 
 // Listen for tab update event
-chrome.tabs.onUpdated.addListener(function(tabId , info) 
-{
+chrome.tabs.onUpdated.addListener(function (tabId, info) {
     refreshIcon({ id: tabId });
 });
